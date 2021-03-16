c2a18a90cb3a66145b3adb3eb46dac5afa216c49
markdrehmann <markdrehmann@gmail.com>
2021-03-15 22:45:53 -0500

    commit tree 1ac82e8d437433e705854badedfb696d6817da70

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_104321) do
+ActiveRecord::Schema.define(version: 2019_05_10_115410) do
 
   create_table "birds", force: :cascade do |t|
     t.string "name"
